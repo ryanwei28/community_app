a809a9e7bd2911b31a17257c97467e48f4fa222b
瑋 <yaha3696369@gmail.com>
2016-11-03 18:02:14 +0800

    announcement

--- announcement.lua
+++ announcement.lua
@@ -15,11 +15,52 @@ local scene = composer.newScene( )
 --宣告各種變數
 --=======================================================================================
 local background
-
+local back 
 --=======================================================================================
 --宣告各種函數函數
 --=======================================================================================
 local init
+local onRowRender
+local onRowTouch
+
+iconData = { 
+    {path =  "" , W = 0 , H = 0 } ,
+    { path = "images/icon.jpg" , W = 140*WIDTH , H = 180*HEIGHT } ,
+    { path = "images/icon.jpg" , W = 140*WIDTH , H = 180*HEIGHT } ,
+    {path =  "" , W = 0 , H = 0 } ,
+    { path = "images/icon.jpg" , W = 140*WIDTH , H = 180*HEIGHT } ,
+    { path = "images/icon.jpg" , W = 140*WIDTH , H = 180*HEIGHT } ,
+    { path = "images/icon.jpg" , W = 140*WIDTH , H = 180*HEIGHT } ,
+    { path = "images/icon.jpg" , W = 140*WIDTH , H = 180*HEIGHT } ,
+    { path = "images/icon.jpg" , W = 140*WIDTH , H = 180*HEIGHT } , 
+    { path = "images/icon.jpg" , W = 140*WIDTH , H = 180*HEIGHT } ,
+}
+
+titleData = {
+    "2016年公告" ,
+    "社區公告標題" ,
+    "社區公告標題" ,
+    "2017年公告" ,
+    "社區公告標題" ,
+    "社區公告標題" ,
+    "社區公告標題" ,
+    "社區公告標題" ,
+    "社區公告標題" ,
+    "社區公告標題" ,
+}
+
+contentData = {
+    "" ,
+    "這裡是社區公告內容。這裡是社區公".."\n".."告內容。這裡是社區公告內容。這裡".."\n".."是社區公告內容。這裡是社區公告內容。" ,
+    "這裡是社區公告內容。這裡是社區公".."\n".."告內容。這裡是社區公告內容。這裡".."\n".."是社區公告內容。這裡是社區公告內容。" ,
+    "" ,
+    "這裡是社區公告內容。這裡是社區公".."\n".."告內容。這裡是社區公告內容。這裡".."\n".."是社區公告內容。這裡是社區公告內容。" ,
+    "這裡是社區公告內容。這裡是社區公".."\n".."告內容。這裡是社區公告內容。這裡".."\n".."是社區公告內容。這裡是社區公告內容。" ,
+    "這裡是社區公告內容。這裡是社區公".."\n".."告內容。這裡是社區公告內容。這裡".."\n".."是社區公告內容。這裡是社區公告內容。" ,
+    "這裡是社區公告內容。這裡是社區公".."\n".."告內容。這裡是社區公告內容。這裡".."\n".."是社區公告內容。這裡是社區公告內容。" ,
+    "這裡是社區公告內容。這裡是社區公".."\n".."告內容。這裡是社區公告內容。這裡".."\n".."是社區公告內容。這裡是社區公告內容。" ,
+    "這裡是社區公告內容。這裡是社區公".."\n".."告內容。這裡是社區公告內容。這裡".."\n".."是社區公告內容。這裡是社區公告內容。" ,
+}
 
 --=======================================================================================
 --定義各種函式
@@ -27,8 +68,68 @@ local init
 init = function ( _parent  )
     background = display.newImageRect( _parent , "images/bg.png", _SCREEN.W , _SCREEN.H )
     background.x , background.y = _SCREEN.CENTER.X , _SCREEN.CENTER.Y
+    background:setFillColor( 0.9 )
 
+    back = display.newText( _parent , "返回", _SCREEN.CENTER.X *0.2, _SCREEN.CENTER.Y*0.1 , font , 20 )
+    back:setFillColor( 0 )
+    back:addEventListener( "tap", function (  )
+        composer.gotoScene( "main_interface" )
+    end )
+
+    tableView = widget.newTableView( {
+        x = _SCREEN.CENTER.X ,
+        y = _SCREEN.CENTER.Y , 
+        width = _SCREEN.W, 
+        height = _SCREEN.H*0.85 , 
+        isBouncedEnabled = true , 
+        hideBackground = false , 
+        onRowRender = onRowRender , 
+        onRowTouch = onRowTouch ,   
+      })   
+
+    for i = 1 , 10 do
+        local rowHeight = 100
+        local rowColor = { default = {0,0,0,0} , over = { 0 , 0.7 , 0.8}}
+        local isCategory = false
+        local lineColor = { 0.8 , 0.8 , 0.8}
+
+        if ( i == 1 or i == 4 ) then
+            lineColor = {1 , 0 , 0}
+            isCategory = true
+            rowHeight = 40 
+            rowColor = {default = { 1 , 1 ,0.2 }}
+        end
+        tableView:insertRow( {
+        rowHeight = rowHeight , 
+        rowColor = rowColor , 
+        isCategory = isCategory , 
+        lineColor = lineColor , 
+      } )
+    end
+   _parent:insert( tableView )
 end
+
+
+
+onRowRender = function ( e , scene)
+   row = e.row
+   
+   icon = display.newImageRect( row , iconData[row.index].path, iconData[row.index].W , iconData[row.index].H )
+   icon.x , icon.y = _SCREEN.CENTER.X*0.28 , 50
+
+   title = display.newText( row , titleData[row.index], _SCREEN.CENTER.X*1.1 , _SCREEN.CENTER.Y*0.1 , font , 23 )
+   title:setFillColor( 0 )
+
+   content = display.newText( row, contentData[row.index], _SCREEN.CENTER.X*0.53, _SCREEN.CENTER.Y*0.28 , font , 14 )
+   content:setFillColor( 0 , 0.4 , 0.8 )
+   content.anchorX = 0
+   content.anchoyY = 0
+end
+
+onRowTouch = function (  )
+    
+end
+
 
 
 --=======================================================================================
